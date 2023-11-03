230e45113cb0a9facc98349a2a952a4d08102c0a
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2023-11-03 22:36:12 +0530

    learned about jsx

--- frontend/app.js
+++ frontend/app.js
@@ -1,36 +1,7 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
-const pareent = React.createElement("div",{id: "parent1"},[
-    React.createElement(
-        "div",
-        { id: "head1" },
-        [
-            ,
-      React.createElement(
-        "h2",
-        { id: "head2" },
-        "Hello World from react2"
-      ),
-      React.createElement(
-        "h3",
-        { id: "head3" },
-        "Hello World from react3"
-      )
-        ]
-      ),
-      React.createElement("div",{id: "child2"},[
-        ,
-      React.createElement(
-        "h2",
-        { id: "head3" },
-        "Hello World from react2"
-      ),
-      React.createElement(
-        "h3",
-        { id: "head4" },
-        "Hello World from react3"
-      )
-      ])
-])
+
+const jsxHeading = <h1>Hello from JSX</h1>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(pareent);
+root.render(jsxHeading);
