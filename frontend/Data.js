565c4bd1befcf367ee853fa739d5e15137401d1d
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2023-11-06 23:28:49 +0530

    create header and food componenets

---
+++ frontend/Data.js
@@ -0,0 +1,655 @@
+export const restaurant  =  [
+  {
+    info: {
+      id: "43411",
+      name: "Khan's Kathi Rolls",
+      cloudinaryImageId: "yfw7njolvs71b4yhjgyr",
+      locality: "Savitri Market Road",
+      areaName: "Sector 18",
+      costForTwo: "₹250 for two",
+      cuisines: ["Fast Food", "Mughlai", "Chinese"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "43411",
+        fees: [
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "BASE_DISTANCE",
+            fee: 8400,
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 8400,
+      },
+      parentId: "117503",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 26,
+        lastMileTravel: 6.6,
+        serviceability: "SERVICEABLE",
+        slaString: "26 mins",
+        lastMileTravelString: "6.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-03 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "UPTO ₹40",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-0a9d6e2e-7443-4272-a4e7-9101d20b9795",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/khans-kathi-rolls-savitri-market-road-sector-18-noida-1-43411",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "205555",
+      name: "The King Of Pizza",
+      cloudinaryImageId: "51ada2658aaafd5a32299885bf6ca9fb",
+      locality: "Baraula",
+      areaName: "Sector 49",
+      costForTwo: "₹400 for two",
+      cuisines: ["Pizzas", "Burgers", "Italian"],
+      avgRating: 3.6,
+      feeDetails: {
+        restaurantId: "205555",
+        fees: [
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "BASE_DISTANCE",
+            fee: 4300,
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4300,
+      },
+      parentId: "15133",
+      avgRatingString: "3.6",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 29,
+        lastMileTravel: 2,
+        serviceability: "SERVICEABLE",
+        slaString: "29 mins",
+        lastMileTravelString: "2.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-04 04:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-0a9d6e2e-7443-4272-a4e7-9101d20b9795",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/the-king-of-pizza-baraula-sector-49-noida-1-205555",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "253710",
+      name: "McDonald's",
+      cloudinaryImageId: "6dc3ed2ca21d71acff7c2a51dfe4c720",
+      locality: "Sector 18",
+      areaName: "Sector 18",
+      costForTwo: "₹400 for two",
+      cuisines: ["American"],
+      avgRating: 4.2,
+      feeDetails: {
+        restaurantId: "253710",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 7800,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 7800,
+      },
+      parentId: "630",
+      avgRatingString: "4.2",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 29,
+        lastMileTravel: 6.6,
+        serviceability: "SERVICEABLE",
+        slaString: "29 mins",
+        lastMileTravelString: "6.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-04 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "UPTO ₹40",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-0a9d6e2e-7443-4272-a4e7-9101d20b9795",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/mcdonalds-sector-18-noida-1-253710",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "233548",
+      name: "Behrouz Biryani",
+      cloudinaryImageId: "89fccaa76f2f760e2742b9e53d32bb69",
+      locality: "Golf Course",
+      areaName: "Sector 45",
+      costForTwo: "₹500 for two",
+      cuisines: [
+        "Biryani",
+        "Lucknowi",
+        "Hyderabadi"
+      ],
+      avgRating: 4.3,
+      feeDetails: {
+        restaurantId: "233548",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5800,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5800,
+      },
+      parentId: "1803",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 28,
+        lastMileTravel: 3.5,
+        serviceability: "SERVICEABLE",
+        slaString: "28 mins",
+        lastMileTravelString: "3.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-03 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-0a9d6e2e-7443-4272-a4e7-9101d20b9795",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/behrouz-biryani-golf-course-sector-45-noida-1-233548",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "20317",
+      name: "Subway",
+      cloudinaryImageId: "1ace5fa65eff3e1223feb696c956b38b",
+      locality: "B Block",
+      areaName: "Sector 50",
+      costForTwo: "₹350 for two",
+      cuisines: ["Salads", "Snacks", "Desserts", "Beverages"],
+      avgRating: 3.8,
+      feeDetails: {
+        restaurantId: "20317",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4800,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4800,
+      },
+      parentId: "2",
+      avgRatingString: "3.8",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 18,
+        lastMileTravel: 0.9,
+        serviceability: "SERVICEABLE",
+        slaString: "18 mins",
+        lastMileTravelString: "0.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-03 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-0a9d6e2e-7443-4272-a4e7-9101d20b9795",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/subway-b-block-sector-50-noida-1-20317",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "583789",
+      name: "Rollsking",
+      cloudinaryImageId: "nfy9vh9kdmc5ayxon8ki",
+      locality: "Gautam Buddha Nagar",
+      areaName: "Gautam Buddha Nagar",
+      costForTwo: "₹300 for two",
+      cuisines: ["North Indian", "Fast Food", "Beverages"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "583789",
+        fees: [
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "BASE_DISTANCE",
+            fee: 4300,
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4300,
+      },
+      parentId: "4697",
+      avgRatingString: "4.1",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 20,
+        lastMileTravel: 2.2,
+        serviceability: "SERVICEABLE",
+        slaString: "20 mins",
+        lastMileTravelString: "2.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-04 07:00:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-0a9d6e2e-7443-4272-a4e7-9101d20b9795",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/rollsking-gautam-buddha-nagar-noida-1-583789",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    info: {
+      id: "31178",
+      name: "Cold Rock Cafe",
+      cloudinaryImageId: "zvcnxmdmprylf0daot0o",
+      locality: "Ach Market",
+      areaName: "Sector 72",
+      costForTwo: "₹250 for two",
+      cuisines: ["Italian", "Snacks", "Chinese", "Ice Cream", "Desserts"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "31178",
+        fees: [
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "BASE_DISTANCE",
+            fee: 5100,
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5100,
+      },
+      parentId: "7131",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 3.9,
+        serviceability: "SERVICEABLE",
+        slaString: "27 mins",
+        lastMileTravelString: "3.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-04 05:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "30% OFF",
+        subHeader: "UPTO ₹75",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          text: "BENEFITS",
+          logo: "v1634558776/swiggy_one/OneLogo_3x.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {
+      context: "seo-data-0a9d6e2e-7443-4272-a4e7-9101d20b9795",
+    },
+    cta: {
+      link: "https://www.swiggy.com/restaurants/cold-rock-cafe-ach-market-sector-72-noida-1-31178",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,84 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
+import {restaurant} from "./Data";
 
-const jsxHeading = <h1>Hello from JSX</h1>
+const Header = ()=>{
+    return(
+        <div className='header'>
+            <div className='logo-container'>
+                <img src="https://marketplace.canva.com/EAE1GTHrBuk/2/0/800w/canva-healthy-food-logo-template.-organic-food-vector-design.-fork%2C-spoon-and-leaves-logotype-L6tNe-eHLzE.jpg" alt="Company Logo" />
+            </div>
+            <div className='details-container'>
+                <ul className='details-list'>
+                    <li className='list'>Home</li>
+                    <li className='list'>About</li>
+                    <li className='list'>Contact Us</li>
+                    <li className='list'>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const Search = ()=>{
+    return(
+        
+            <div className='search-container'>
+                <input type="text" placeholder='Search'/>
+                <button className='search-btn'>Search</button>
+            </div>  
+      
+    )
+}
+
+
+const RestaurantCard = (props)=>{
+   const {resData} = props;
+   console.log(resData);
+    return (
+        <>
+           {/* food card */}
+           <div className='body-container'>
+                <div className='foodcardlogo-container'>
+                    <img className="comany-name"  src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + resData.cloudinaryImageId} alt="dominos" />
+                    
+                        <h3 className='heading3'>{resData.name}</h3>
+                        <h4 className='heading3'>{resData.cuisines.join(',')}</h4>
+                        <h4 className='heading3'>{resData.areaName}</h4>
+                        <h4 className='heading3'>{resData.avgRating}</h4>
+                </div>
+           </div>
+        </>
+    )
+}
+
+const Body =()=>{
+    return(
+        <div className='mainbody-container'>
+            {
+                restaurant.map((restaurants,index)=>(
+                    <RestaurantCard key={restaurants.info.id} resData={restaurants.info}/>
+                ))
+            }
+
+            
+        </div>
+    )
+}
+
+const App = ()=>{
+    return(
+        <>
+            <Header/>
+            <Search/>
+            <Body/>
+            
+
+        </>
+    )
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<App/>);
