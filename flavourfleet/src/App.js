22b8a9e4aa3054cb65ebd97960de13f82cacc9d7
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-01-14 20:31:43 +0530

    initial setup

--- flavourfleet/src/App.js
+++ flavourfleet/src/App.js
@@ -1,47 +1,7 @@
-import Header from "./components/Header";
-import Body from "./components/Body";
-import { createBrowserRouter, Outlet } from "react-router-dom";
-import About from "./components/About";
-import ContactUs from "./components/ContactUs";
-import Cart from "./components/Cart";
-import SignIn from "./components/SignIn";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Outlet />
-    </div>
-  );
+  return <h1>Hi</h1>;
 }
-
-export const AppRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Body />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contactUs",
-        element: <ContactUs />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/signin",
-        element: <SignIn />,
-      },
-    ],
-  },
-]);
 
 export default App;
--- flavourfleet/src/index.js
+++ flavourfleet/src/index.js
@@ -1,11 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { RouterProvider } from "react-router";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import App, { AppRouter } from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={AppRouter}>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </RouterProvider>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
