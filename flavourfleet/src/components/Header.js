449c2d7174031a68681d3b09b3da365666b014fd
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-01-14 21:27:58 +0530

    Added Header

---
+++ flavourfleet/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Logo from "../Assets/logo.png";
+const Header = () => {
+  return (
+    <>
+      <div className="flex items-center">
+        <div>
+          <img src={Logo} alt="Company Logo" className="h-[80px] ml-[35px]" />
+        </div>
+        <div className="flex-grow">
+          <ul className="flex justify-end">
+            <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
+              Home
+            </li>
+            <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
+              About
+            </li>
+            <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
+              Cart
+            </li>
+            <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
+              User
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
