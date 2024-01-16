920727b5439374706fd94bd3a82f34e0d38cca90
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-01-17 00:28:10 +0530

    Added Search Functionality

--- flavourfleet/src/components/RestaurantMenuList.js
+++ flavourfleet/src/components/RestaurantMenuList.js
@@ -5,7 +5,16 @@ import { SWIGGY_API } from "../utils/Constants";
 
 const RestaurantMenuList = () => {
   const [restaurantData, setRestaurantData] = useState([]);
-  console.log("Initial Return");
+  const [searchValue, setSearchValue] = useState("");
+  const [filterSearch, setFilterSearch] = useState([]);
+  // Search Data
+  const filterSearchData = (val) => {
+    console.log(val);
+    const filteredRestaurant = restaurantData.filter((res) =>
+      res.info.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilterSearch(filteredRestaurant);
+  };
   useEffect(() => {
     const fetchRestaurantData = async () => {
       try {
@@ -15,21 +24,45 @@ const RestaurantMenuList = () => {
           json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
             ?.restaurants
         );
+        setFilterSearch(
+          json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+            ?.restaurants
+        );
       } catch (error) {
         console.error("Error fetching restaurant data:", error);
       }
     };
-    console.log("Inside UseEffect");
     fetchRestaurantData();
   }, []);
-
-  console.log("Before Return");
   return (
-    <div className="flex mt-[50px] ml-[15px] justify-center flex-wrap">
-      {restaurantData.map((resData) => (
-        <RestaurantCardLayout resData={resData} key={resData.info.id} />
-      ))}
-    </div>
+    <>
+      {/* search Container */}
+      <div className="ml-[40%] mt-[20px]">
+        <input
+          className="border border-black w-[300px] mr-[32px] p-[7px] rounded-lg"
+          type="text"
+          placeholder="Search..."
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
+        />
+        <button
+          className="h-[36px] w-[80px] border border-black rounded-lg"
+          onClick={() => {
+            filterSearchData(searchValue);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      {/* Restaurant container */}
+      <div className="flex mt-[50px] ml-[15px] justify-center flex-wrap">
+        {filterSearch.map((resData) => (
+          <RestaurantCardLayout resData={resData} key={resData.info.id} />
+        ))}
+      </div>
+    </>
   );
 };
 
