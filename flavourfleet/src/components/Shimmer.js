ffc71886688bbb75f3fb01bcbf74909589451502
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-01-17 00:40:20 +0530

    fix warning

--- flavourfleet/src/components/Shimmer.js
+++ flavourfleet/src/components/Shimmer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Shimmer = () => {
   return (
     <div className="flex flex-wrap">
-      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] mt-[12px] animate-pulse">
+      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px]animate-pulse">
         <div className="h-[150px] w-[250px] bg-gray-300"></div>
         <div className="mt-[12px]">
           <div className="h-[20px] w-[200px] bg-gray-300 mb-2"></div>
@@ -14,7 +14,7 @@ const Shimmer = () => {
         </div>
       </div>
 
-      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] mt-[12px] animate-pulse">
+      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px]  animate-pulse">
         <div className="h-[150px] w-[250px] bg-gray-300"></div>
         <div className="mt-[12px]">
           <div className="h-[20px] w-[200px] bg-gray-300 mb-2"></div>
@@ -25,7 +25,7 @@ const Shimmer = () => {
         </div>
       </div>
 
-      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] mt-[12px] animate-pulse">
+      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px]  animate-pulse">
         <div className="h-[150px] w-[250px] bg-gray-300"></div>
         <div className="mt-[12px]">
           <div className="h-[20px] w-[200px] bg-gray-300 mb-2"></div>
@@ -36,7 +36,7 @@ const Shimmer = () => {
         </div>
       </div>
 
-      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] mt-[12px] animate-pulse">
+      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] animate-pulse">
         <div className="h-[150px] w-[250px] bg-gray-300"></div>
         <div className="mt-[12px]">
           <div className="h-[20px] w-[200px] bg-gray-300 mb-2"></div>
@@ -47,7 +47,7 @@ const Shimmer = () => {
         </div>
       </div>
 
-      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] mt-[12px] animate-pulse">
+      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] animate-pulse">
         <div className="h-[150px] w-[250px] bg-gray-300"></div>
         <div className="mt-[12px]">
           <div className="h-[20px] w-[200px] bg-gray-300 mb-2"></div>
@@ -58,7 +58,7 @@ const Shimmer = () => {
         </div>
       </div>
 
-      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] mt-[12px] animate-pulse">
+      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] animate-pulse">
         <div className="h-[150px] w-[250px] bg-gray-300"></div>
         <div className="mt-[12px]">
           <div className="h-[20px] w-[200px] bg-gray-300 mb-2"></div>
@@ -69,7 +69,7 @@ const Shimmer = () => {
         </div>
       </div>
 
-      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] mt-[12px] animate-pulse">
+      <div className="relative border-solid border-2 w-[300px] h-[350px] p-[22px] ml-[90px] mt-[120px] mr-[25px] animate-pulse">
         <div className="h-[150px] w-[250px] bg-gray-300"></div>
         <div className="mt-[12px]">
           <div className="h-[20px] w-[200px] bg-gray-300 mb-2"></div>
