38af1938e39b4f3ef56a481d0bece01bc2da5c60
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-01-18 01:06:07 +0530

    Added routes for header section components

--- flavourfleet/src/App.js
+++ flavourfleet/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import About from "./components/About";
+import Cart from "./components/Cart";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import RestaurantMenuList from "./components/RestaurantMenuList";
 import { createBrowserRouter, Outlet } from "react-router-dom";
+import User from "./components/User";
 function App() {
   return (
     <>
@@ -19,6 +23,22 @@ export const AppRouter = createBrowserRouter([
       {
         path: "/",
         element: <RestaurantMenuList />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/user",
+        element: <User />,
       },
     ],
   },
--- flavourfleet/src/components/Header.js
+++ flavourfleet/src/components/Header.js
@@ -1,30 +1,41 @@
 import React from "react";
 import Logo from "../Assets/logo.png";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
       <div className="flex items-center shadow-lg relative z-10 bg-gradient-to-b from-transparent to-green">
         <div>
-          <img
-            src={Logo}
-            alt="Company Logo"
-            className="h-[80px] ml-[35px] p-[7px] cursor-pointer"
-          />
+          <Link to="/">
+            <img
+              src={Logo}
+              alt="Company Logo"
+              className="h-[80px] ml-[35px] p-[7px] cursor-pointer"
+            />
+          </Link>
         </div>
         <div className="flex-grow">
           <ul className="flex justify-end">
-            <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
-              Home
-            </li>
-            <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
-              About
-            </li>
-            <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
-              Cart
-            </li>
-            <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
-              User
-            </li>
+            <Link to="/home">
+              <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
+                Home
+              </li>
+            </Link>
+            <Link to="/about">
+              <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
+                About
+              </li>
+            </Link>
+            <Link to="/cart">
+              <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
+                Cart
+              </li>
+            </Link>
+            <Link to="/user">
+              <li className="mr-[50px] cursor-pointer transition-transform transform hover:scale-125">
+                User
+              </li>
+            </Link>
           </ul>
         </div>
       </div>
