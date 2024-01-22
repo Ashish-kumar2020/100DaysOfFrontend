b3797498dd8cfe1826cd81b7aa980dcb34b8998b
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-01-22 23:10:24 +0530

    update ui

--- DOM/Todolist/script.js
+++ DOM/Todolist/script.js
@@ -2,6 +2,7 @@ const inputTextArea = document.getElementById("input-textarea");
 const todoList = document.getElementById("todos-list");
 
 let currentTodo;
+
 function enterTodo() {
   const inputValue = inputTextArea.value;
   currentTodo = inputValue;
@@ -10,15 +11,32 @@ function enterTodo() {
 function addTodo() {
   // create a dynamic ul
   const unorderList = document.createElement("ul");
+  unorderList.classList.add("todo-item"); // Adding a class to the <ul> element
+
   const listItem = document.createElement("li");
+  listItem.classList.add("list-item"); // Adding a class to the <li> element
+
   const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-button"); // Adding a class to the <button> element
   deleteBtn.textContent = "Delete";
+
+  const editButton = document.createElement("button");
+  editButton.classList.add("delete-button");
+  editButton.textContent = "Edit Todo";
+
   listItem.textContent = currentTodo;
+  inputTextArea.value = currentTodo;
 
   deleteBtn.addEventListener("click", () => {
     deleteTodo(unorderList);
   });
+
+  editButton.addEventListener("click", () => {
+    editTodo(inputTextArea, listItem, unorderList);
+  });
+
   unorderList.appendChild(deleteBtn);
+  unorderList.appendChild(editButton);
   todoList.appendChild(unorderList);
   unorderList.appendChild(listItem);
 
@@ -28,4 +46,8 @@ function addTodo() {
 
 function deleteTodo(parentList) {
   todoList.removeChild(parentList);
+}
+
+function editTodo(inputTextArea, listItem, unorderList) {
+  inputTextArea.value = listItem.textContent;
 }
